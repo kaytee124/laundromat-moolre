aa5bf7c8b398d9e9fd331edbd156d03ca1d06d67
kaytee124 <ktproductions@gmail.com>
2026-07-11 14:20:29 +0000

    default password changed

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   ORDER_IN_PROGRESS_PAYMENT_RATIO: 0.3,
-  DEFAULT_CUSTOMER_PASSWORD: 'ChangeMe123!',
+  DEFAULT_CUSTOMER_PASSWORD: 'moolre123!',
   ROLES: ['superadmin', 'admin', 'employee', 'client'],
   ORDER_STATUSES: ['pending', 'in_progress', 'ready', 'completed', 'cancelled'],
   PAYMENT_STATUSES: ['pending', 'partially_paid', 'paid'],
